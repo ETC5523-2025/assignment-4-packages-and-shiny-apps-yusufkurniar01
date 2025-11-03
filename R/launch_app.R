6304a21b8e84103e52a567e2c2fb664c11814407
Yusuf Romadhon <yrom0002@student.monash.edu>
2025-11-03 13:56:31 +1100

    updating description, checking overall documents, updating vignettes, updating gitignore

--- R/launch_app.R
+++ R/launch_app.R
@@ -4,10 +4,11 @@
 #' @return No return value, and called for side effects.
 #' @export
 launch_app <- function() {
-  #resolve the app directory inside inst/
+  #resolve the app directory inside inst/app
   app_dir <- system.file("app", package = "yusufHAIGermany")
   if (app_dir == "" || !dir.exists(app_dir)) {
-    stop("App directory not found. Build/install the package, then try again.", call. = FALSE)
+    stop("App directory not found. Build/install the package, then try again.",
+         call. = FALSE)
   }
 
   #run the Shiny app
